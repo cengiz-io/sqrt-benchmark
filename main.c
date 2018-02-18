b388a4f65431eb707cfbd99aee4d2b7b4c2233ac
Cengiz Can <123910+cengizIO@users.noreply.github.com>
2018-02-18 20:20:56 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,24 +8,17 @@ static const int TEST_COUNT = 4 * 1000 * 1000;
 static const int MAX_NUMBER = 128 * 1000;
 
 int main(void) {
-    FILE *dev_null_ptr = fopen("/dev/null", "w");
-
-    if (dev_null_ptr == NULL)
-        return -69;
-
     srand((unsigned int) time(NULL));
 
     MEASURE_ELAPSED_TIME("fast", {
         for (int i = 0; i < TEST_COUNT; i++) {
-            double val = fast_sqrt(rand() % MAX_NUMBER);
-            fprintf(dev_null_ptr, "%f", val);
+            volatile double val = fast_sqrt(rand() % MAX_NUMBER);
         }
     });
 
     MEASURE_ELAPSED_TIME("gnu", {
         for (int i = 0; i < TEST_COUNT; i++) {
-            double val = 1 / sqrt(rand() % MAX_NUMBER);
-            fprintf(dev_null_ptr, "%f", val);
+            volatile double val = 1 / sqrt(rand() % MAX_NUMBER);
         }
     });
 }
